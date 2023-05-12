ed97cf678a842e2133a9d34f961a4fe9f1e04c42
zkemii <zkgaren@gmail.com>
2023-05-12 14:07:48 -0300

    first commit

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+# dovelop stage
+FROM node:17.8.0-alpine3.14
+WORKDIR /app
+COPY frontend/package*.json ./
+RUN npm install
+RUN npm install -g @vue/cli
+COPY . .
---
+++ bin/create_project.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker compose exec backend composer self-update
+
+docker compose exec backend composer create-project --prefer-dist yiisoft/yii2-app-basic .
