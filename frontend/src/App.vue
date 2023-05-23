5dc9f3092bf36ba4bf2d21af55930d21ae942750
zkemii <zkgaren@gmail.com>
2023-05-23 18:17:21 -0300

    Error Vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <main-menu></main-menu>
+    <main-bar></main-bar>
+    <v-main>
+      <v-container fluid>
+        <router-view/>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainMenu from './components/MainMenu.vue'
+import MainBar from './components/MainBar.vue'
 
-export default {
+export default{
   name: 'App',
+
   components: {
-    HelloWorld
-  }
+    MainMenu,
+    MainBar
+  },
+
+  data: () => ({
+    //
+    showMenu: true,
+    items: [
+      {title: 'Home', icon: 'mdi-view-dashboard'}
+    ],
+  }),
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+//import vuetify from './plugins/vuetify'
+//import router from './plugins/router'
 
-createApp(App).mount('#app')
+
+//Vue.config.productionTip = false
+
+new Vue({
+ // vuetify,
+  //router,
+  render: h => h(App),
+}).$mount('#app')
+
